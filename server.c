d5ad456633aa1d67cc799e5f03678b6234244aa8
Egor Kritsky <mrakobes14881@gmail.com>
2024-05-31 16:14:25 +0500

    First tryings.

--- server.c
+++ server.c
@@ -49,40 +49,72 @@ struct server *init_server()
 }
 void handle_command(char* command) {
     if (strcmp(command, "test_connection") == 0) {
-        // Обработка команды test_connection
+
         printf("Received test_connection command\n");
-    } else if (strcmp(command, "play") == 0) {
-        // Обработка команды play
+    } 
+	else if (strcmp(command, "play") == 0) {
+		//system("mpv --fs --playlist=<(find '/home/krinyo/playlist' -type f -name '*')");
+		system("setsid sh -c 'bash mpv --fs --playlist=<(find \"/home/krinyo/playlist\" -type f -name \"*\") <> /dev/tty3 >&0 2>&1'");
         printf("Received play command\n");
-    } else if (strcmp(command, "restart") == 0) {
-        // Обработка команды restart
+    } 
+	else if (strcmp(command, "restart") == 0) {
+
         printf("Received restart command\n");
-    } else if (strcmp(command, "stop") == 0) {
-        // Обработка команды stop
+    } 
+	else if (strcmp(command, "stop") == 0) {
+
         printf("Received stop command\n");
-    } else {
+    } 
+	else {
         printf("Unknown command: %s\n", command);
     }
 }
 
-int main()
-{
-	struct server *ms = init_server();
-	while(ms->desc_count < MAX_CONNECTIONS){
-		if((ms->client_desc = accept(ms->server_socket, NULL, NULL)) != -1){
-			printf("Accepted!\n");
-			ms->desc_count ++;
-		}
-	}
-
+int main() {
+    struct server *ms = init_server();
     char buff[256];
-    while(1){
-        read(ms->client_desc, buff, sizeof(buff)-1);
-        handle_command(buff);
-        memset(buff, '1', sizeof(buff));
-    }
-    /*close*/
-    close(ms->client_desc);
-    ms->desc_count --;
 
+    while(1) { // Бесконечный цикл для принятия соединений
+        if((ms->client_desc = accept(ms->server_socket, NULL, NULL)) != -1){
+            printf("Accepted!\n");
+
+			read(ms->client_desc, buff, sizeof(buff)-1);
+			printf("%s\n", buff);
+			//char* trimmed_buffer = strtok(buff, "\0 ");
+			handle_command(buff);
+			//memset(buff, '0', sizeof(buff));
+
+            /*close*/
+            close(ms->client_desc);
+            printf("Connection closed\n");
+        }
+    }
 }
+
+/*
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+    CURL *curl;
+    FILE *fp;
+    int result;
+    char *url = "http://localhost:8000/download_playlist/playlist1/";
+    char outfilename[FILENAME_MAX] = "downloaded_playlist.zip";
+    curl = curl_easy_init();
+    if (curl)
+    {
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        result = curl_easy_perform(curl);
+        //always cleanup 
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+    return 0;
+}
+
+*/
